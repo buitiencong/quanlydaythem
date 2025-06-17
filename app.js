ad4ca84466c0d5ecf0172e91a9a838698628a114
buitiencong <buitiencong@gmail.com>
2025-06-17 15:36:49 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -85,16 +85,16 @@ window.addEventListener('beforeinstallprompt', (e) => {
 if (!isRunningStandalone() && /iPhone|iPad|iPod/.test(navigator.userAgent)) {
   setTimeout(() => {
 showToast(`
-  📱 Thêm ứng dụng vào màn hình chính<br>
+  📱 Thêm ứng dụng vào màn hình chính:<br>
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin: 0 6px;">
     <path d="M12 2v13"/><path d="m16 6-4-4-4 4"/><path d="M4 12v8a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2v-8"/>
   </svg>
-  ‘Chia sẻ’ → 
+  Chia sẻ → 
   <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin: 0 6px;">
     <rect width="18" height="18" x="3" y="3" rx="2"/>
     <path d="M8 12h8"/><path d="M12 8v8"/>
   </svg>
-  ‘Thêm vào Màn hình chính’
+  Thêm vào Màn hình chính
 `);
 
   }, 1500);
@@ -111,7 +111,7 @@ function showToast(message, svgIcon = '') {
       bottom: 20px;
       left: 50%;
       transform: translateX(-50%);
-      min-width: 280px;
+      min-width: 320px;
       max-width: 90%;
       background: #007acc;
       color: white;
